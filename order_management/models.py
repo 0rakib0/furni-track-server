6d5c6dced7da7defb0cd70950ed522c6e1b77527
rakib00 <hassanrakibul926@gmail.com>
2025-08-11 02:32:16 +0600

    create employee model

--- order_management/models.py
+++ order_management/models.py
@@ -7,4 +7,7 @@ class OrderModel(models.Model):
     memo_number = models.CharField(max_length=20)
     cutomar_phone = models.CharField(max_length=20)
     delivery_address = models.TextField()
+    delivery_date = models.DateField()
+    frame_show_date = models.DateField()
+    dealer = models.ForeignKey(on_delete=models.DO_NOTHING)
     
--- utilities/models.py
+++ utilities/models.py
@@ -8,6 +8,26 @@ class DealerModel(models.Model):
     phone = models.CharField(max_length=20)
     address = models.TextField()
     dealer_image = models.ImageField(upload_to='dealer_image')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
+class EmployeeModel(models.Model):
+    name = models.CharField(max_length=166)
+    age = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    position = models.CharField(max_length=40)
+    join_date = models.DateField()
+    performance_status = models.CharField(max_length=30)
+    address = models.TextField()
+    employee_image = models.ImageField(upload_to='dealer_image')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     
     def __str__(self):
