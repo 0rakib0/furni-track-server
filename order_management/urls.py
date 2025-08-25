46fdc697a2212c3ec08908c23f95cf0f30204459
rakib00 <hassanrakibul926@gmail.com>
2025-08-26 01:41:05 +0600

    create model serializer and also create modelviewset for order

--- order_management/urls.py
+++ order_management/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+from . import views
+
+
 
 urlpatterns = [
+    
 ]
+
+router = DefaultRouter()
+router.register(r'orders', views.OrderViewSet, basename='orders')
+urlpatterns = router.urls
--- order_management/views.py
+++ order_management/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import OrderModel
+from .serializers import OrderSerializer
 # Create your views here.
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    queryset = OrderModel.objects.all()
