6d66a1f2f9c35df1862e031ca568f29f1748f5c7
rakib00 <hassanrakibul926@gmail.com>
2025-09-01 01:05:31 +0600

    create customar serializer

--- order_management/serializers.py
+++ order_management/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import OrderModel
+from .models import OrderModel, CustomerModel
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderModel
         fields = '__all__'
+        
+class CustomarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerModel
+        fields = '__all__'
+  
   
--- order_management/views.py
+++ order_management/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .models import OrderModel
+from .models import OrderModel, CustomerModel
 from .serializers import OrderSerializer
 # Create your views here.
 
