f9781107d251478e31bd6f686cc2b889faf308d0
rakib00 <hassanrakibul926@gmail.com>
2025-09-02 02:08:47 +0600

    update order serializer for add order

--- order_management/serializers.py
+++ order_management/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import OrderModel, CustomerModel
+from utilitie.models import DealerModel, EmployeeModel
 from utilitie.serializers import DealerSerializer, EmployeeSerializer
 
 class CustomarSerializer(serializers.ModelSerializer):
@@ -11,6 +12,18 @@ class OrderSerializer(serializers.ModelSerializer):
     customar = CustomarSerializer(read_only=True)
     dealer = DealerSerializer(read_only=True)
     employee = EmployeeSerializer(read_only=True)
+    
+    customar_id = serializers.PrimaryKeyRelatedField(
+        queryset=CustomerModel.objects.all(), source='customar', write_only=True
+    )
+    dealer_id = serializers.PrimaryKeyRelatedField(
+        queryset=DealerModel.objects.all(), source='dealer', write_only=True
+    )
+    employee_id = serializers.PrimaryKeyRelatedField(
+        queryset=EmployeeModel.objects.all(), source='employee', write_only=True
+    )
+    
+    
     class Meta:
         model = OrderModel
         fields = '__all__'
