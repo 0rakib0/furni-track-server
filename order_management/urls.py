5bd5fd921fa765d20147edb90e950084e51e5d6f
rakib00 <hassanrakibul926@gmail.com>
2025-09-03 02:43:02 +0600

    create order management endpoint for get dashbord data

--- order_management/urls.py
+++ order_management/urls.py
@@ -1,15 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
 from . import views
-
-
-
-urlpatterns = [
-    
-]
 
 router = DefaultRouter()
 router.register(r'orders', views.OrderViewSet, basename='orders')
 router.register(r'customars', views.CustomarViewSet, basename='customar')
 urlpatterns = router.urls
+
+
+urlpatterns = [
+    path('order-management/', views.OrderManagementDashbord.as_view()),
+     path('', include(router.urls)),
+]
+
--- order_management/views.py
+++ order_management/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import OrderModel, CustomerModel
 from .serializers import OrderSerializer, CustomarSerializer
+from rest_framework.views import APIView
 # Create your views here.
 
 
@@ -13,3 +14,21 @@ class OrderViewSet(viewsets.ModelViewSet):
 class CustomarViewSet(viewsets.ModelViewSet):
     serializer_class = CustomarSerializer
     queryset = CustomerModel.objects.all()
+    
+    
+class OrderManagementDashbord(APIView):
+    def get(self, request):
+        # get latest 10 data
+        recent_order = OrderModel.objects.all().order_by('-id')[:10]
+        recent_serializer = OrderSerializer(recent_order, many=True)
+        
+        # get only pending order data
+        pending_order = OrderModel.objects.filter(order_status=False).order_by("-id")[:10]
+        pending_serializer = OrderSerializer(pending_order, many=True)
+        return Response({
+            'recent_order':recent_serializer.data,
+            'pending_order':pending_serializer.data
+        })
+    
+    
+
