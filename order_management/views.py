3e2afc289a09af9f1fc54922763323d8b004e0e3
rakib00 <hassanrakibul926@gmail.com>
2025-11-02 08:17:25 +0600

    design bar chart and get data fro backend for chart

--- order_management/views.py
+++ order_management/views.py
@@ -85,15 +85,16 @@ def ChartData(request):
             .annotate(count=Count('id'))
             .order_by('date')
         )
-        ontime_delivery_data = (
-            OrderModel.objects
-            .filter(order_status=True, delivery_date=F('delivery_date'))
-            .annotate(date=TruncDate("delivery_date"))
-            .values("date")
-            .annotate(count=Count("id"))
-            .order_by("date")
-        )
-        return Response({"order_data":order_data, "ontime_delivery_data":ontime_delivery_data}, status=status.HTTP_200_OK)
+        
+        # ontime_delivery_data = (
+        #     OrderModel.objects
+        #     .filter(order_status=True, delivery_date=F('delivery_date'))
+        #     .annotate(date=TruncDate("delivery_date"))
+        #     .values("date")
+        #     .annotate(count=Count("id"))
+        #     .order_by("date")
+        # )
+        return Response({'order_data':order_data}, status=status.HTTP_200_OK)
     except Exception as e:
         return Response ({"error":str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
